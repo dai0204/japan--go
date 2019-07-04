b88e8c211e2a388d51ac5ce74c559296ace7b367
dai0204 <nandai0204@gmail.com>
2019-07-04 16:19:48 +0900

    root_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  devise_for :users
  root 'tweets#index'
+ post "tweets/new" => "tweets#create"
  resources :tweets do
   resources :comments, only: [:create]
  end
